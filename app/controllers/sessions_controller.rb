da9ba6703192ec229ead9047b70c5ff6a2b27096
gt-sdi <gto.sdi@gmail.com>
2012-03-22 12:40:41 -0600

    Refactor Account ... Add logging of auth refresh.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     ENV['sfdc_refresh_token'] = request.env['omniauth.auth']['credentials']['refresh_token']
     ENV['sfdc_instance_url'] = request.env['omniauth.auth']['credentials']['instance_url']
     ENV['sf_update_time'] = Time.zone.now.to_s
+    logger.info "Logged in to Sales force"
     redirect_to :action => "index"
   end
 end
